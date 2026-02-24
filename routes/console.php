df84f79b1560ac7d8681ff216212d9ca8be21566
Null-ch <zuxobot@yandex.ru>
2026-02-24 08:11:07 +0300

    фикс команд

--- routes/console.php
+++ routes/console.php
@@ -1,20 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
-
-Schedule::command('app:weather')
-    ->timezone('Europe/Moscow')
-    ->dailyAt('07:00');
-
-Schedule::command('app:weather')
-    ->timezone('Europe/Moscow')
-    ->dailyAt('16:00');
 
 Schedule::command('app:weekly-reaction-stats')
     ->timezone('Europe/Moscow')
